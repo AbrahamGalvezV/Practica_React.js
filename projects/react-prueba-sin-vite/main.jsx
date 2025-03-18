a51e6e483f9399a36f6538a1dae2b906938b6bce
AbrahamGalvezV <2abrahamtattoosk8@gmail.com>
2025-03-18 18:55:17 +0100

    document update and create styles

--- projects/react-prueba-sin-vite/main.jsx
+++ projects/react-prueba-sin-vite/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "../stc/App";
+import { App } from "./src/App";
 
 const root = createRoot(document.getElementById('app')) // Renderizamos app dentro de root 
 
